cb02b7617fa091eeae1f5fbcdcdfd42816fbad03
abdulaziz <abdulaziz@gmail.com>
2022-08-20 17:15:32 +0500

    my portofilo

---
+++ typed.js
@@ -0,0 +1,10 @@
+// let typed = new Typed('.type', {
+//     strings: [
+//         'frontend', 
+//         'developer',
+//         'Javascript'
+//                 ],
+//         typeSpeed: 60,        
+//         backSpeed: 60,        
+//         loop: true        
+//   });
